c134d6f2f4099956100896d53be0b457c4738af5
Hidden-Node <mr.regista6@gmail.com>
2026-04-29 20:03:30 +0330

    fix: add missing Arrangement import in HomeScreen

--- android/app/src/main/java/com/gooserelay/gooserelayvpn/ui/home/HomeScreen.kt
+++ android/app/src/main/java/com/gooserelay/gooserelayvpn/ui/home/HomeScreen.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
